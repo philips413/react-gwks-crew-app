e799e0eca500cedc95d72b80bf5980d3e1517b2c
Soobin_L <toqurrkatjd08@gmail.com>
2022-04-14 21:13:12 +0900

    feature: Mypage 내가 가입한 크루 list 표기(오류)

---
+++ src/components/CrewCard.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+import {Badge, Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle, Col, Row} from "reactstrap";
+import CrewMemberCard from "./UserCard";
+import NoImage from "../assets/img/no-image-found-360x250-1-300x208.png";
+import CommunityBadgeList from "./CommunityBadgeList";
+
+interface CrewCardProps {
+    id: number;
+    name: string;
+    abstract: string;
+    image_thumbnail: string;
+    meeting_type: string;
+    meeting_time: string;
+    meeting_limit: string;
+    member_limit: number;
+}
+
+const CrewCart = styled(Card)`
+  margin-bottom: 10px;
+`
+
+const HashSpan = styled.span`
+  color: #5899fa;
+  font-size: 13px;
+  cursor: pointer;
+  :active {
+    text-decoration: underline;
+  }
+`
+
+export const CrewCard = (props: CrewCardProps) => {
+    return (
+        <React.Fragment>
+            <CrewCart key={`card${props.id}`}>
+                <CardBody>
+                    <Row
+                        key={`cardRow${props.id}`}
+                        xs={2}
+                    >
+                        <Col xs={3}>
+                            <img
+                                alt={""}
+                                src={props.image_thumbnail || NoImage}
+                                style={{"width": "80px", "height": "80px"}}
+                            />
+                        </Col>
+                        <Col xs={9}>
+                            <CardTitle
+                                tag="h5"
+                            >
+                                {props.name}
+                            </CardTitle>
+                            <CardSubtitle
+                                className="mb-2 text-muted"
+                                tag="h6"
+                            >
+                                {props.abstract}&emsp;
+                            </CardSubtitle>
+                        </Col>
+                    </Row>
+                    <CardText>
+                        {props.meeting_time ? (<HashSpan color={"primary"}>#{props.meeting_time}</HashSpan>) : ''}&nbsp;
+                        {props.meeting_type ? (<HashSpan color={"success"}>#{props.meeting_type}</HashSpan>) : ''}&nbsp;
+                        {props.member_limit ? (<HashSpan color={"success"}>#선착순{props.member_limit}명</HashSpan>) : ''}
+                    </CardText>
+                </CardBody>
+            </CrewCart>
+        </React.Fragment>
+    );
+}
+
+export default CrewCard;
--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,14 +1,31 @@
 import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
-import {Alert, Button, Card, CardBody, CardText} from "reactstrap";
+import {Alert, Badge, Button, Card, CardBody, CardHeader, CardText} from "reactstrap";
 import { getUserDetail } from "../../api/UserApi";
 import { StorageUtil } from "../../config/BrowserUtil";
 import { CommunityCode } from "../../enum/OperationCode";
 import Header from "../../layout/Header";
 import { PageTagProps } from "../interface/PageInterface";
+import {getCrewDetail, getCrewList} from "../../api/CrewApi";
+import styled from "styled-components";
+import {TiUserAdd} from "react-icons/ti";
+import NoImage from "../../assets/img/no-image-found-360x250-1-300x208.png";
+import CommunityBadgeList from "../../components/CommunityBadgeList";
+import CrewCard from "../../components/CrewCard";
+import CrewMemberCard from "../../components/CrewMemberCard";
+
 
 const FindCommunity = (code: number) => {
     return (CommunityCode.findById(code).text);
+}
+
+const getCardList = (list: any) => {
+    const vaildCrewMember = (members: []) => {
+        const userId = StorageUtil.session.getId();
+        const filterMembers = members.filter(item => item === Number(userId));
+        if (filterMembers.length > 0) {
+        }
+    }
 }
 
 const MainPage = (props: PageTagProps) => {
@@ -16,6 +33,11 @@ const MainPage = (props: PageTagProps) => {
     const status = StorageUtil.session.getItem("status");
     const [userInfo, setUserInfo] = useState({} as any);
     const userId = StorageUtil.session.getId();
+    const [crew, setCrew] = useState({
+        id: 0,
+        name: [],
+        members: [],
+    } as any);
 
     useEffect(() => {
         const accessToken = StorageUtil.session.getAccessToken()
@@ -27,7 +49,15 @@ const MainPage = (props: PageTagProps) => {
             const userData = await getUserDetail(Number(userId));
             setUserInfo(userData);
         }
+
+        const fetchCrewData = async function() {
+            const data = await getCrewList();
+            setCrew(data);
+        }
+
         fetchUserData();
+        fetchCrewData();
+
     }, []);
 
     const doJoin = () => {
@@ -38,6 +68,24 @@ const MainPage = (props: PageTagProps) => {
         const req = StorageUtil.session.getItem("req");
         const res = StorageUtil.session.getItem("res");
         alert(`${req}\n================\n${res}`)
+    }
+
+    const GetCardList = (crew: any) => {
+        console.log(crew);
+        let filterMembers = crew.filter((item: any) => item === Number(userId));
+        if (filterMembers.length > 0) {
+            return ( <CrewCard
+                id={crew.id}
+                name={crew.name}
+                abstract={crew.abstract}
+                image_thumbnail={crew.image_thumbnail}
+                meeting_type={crew.meeting_type}
+                meeting_time={crew.meeting_time}
+                meeting_limit={crew.meeting_limit}
+                member_limit={crew.member_limit}
+            />)
+        }
+        else return <b> 아직 가입한 크루가 없네요 :( </b>
     }
 
     return (
@@ -88,6 +136,14 @@ const MainPage = (props: PageTagProps) => {
                                     내 정보 수정
                                 </Button>
                             </div>
+                        </CardBody>
+                    </Card>
+                    <Card style={{"marginTop": "10px"}}>
+                        <CardHeader>
+                            내가 가입한 크루
+                        </CardHeader>
+                        <CardBody>
+                            {GetCardList(crew)}
                         </CardBody>
                     </Card>
                 </div>
