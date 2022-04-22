390bcde49993e03f9dc57c6ed4433b9d714168ff
Soobin_L <toqurrkatjd08@gmail.com>
2022-04-23 02:19:15 +0900

    fix: 주석제거 및 수정

--- src/pages/crew/CrewListPage.tsx
+++ src/pages/crew/CrewListPage.tsx
@@ -5,8 +5,6 @@ import { PageTagProps } from "../interface/PageInterface";
 import UserCard from "../../components/UserCard";
 import styled from "styled-components";
 import {Button, ButtonGroup, CardBody} from "reactstrap";
-import {StorageUtil} from "../../config/BrowserUtil";
-import {TiUserAdd} from "react-icons/ti";
 
 
 const SelCommunity = styled.div`
@@ -31,7 +29,6 @@ const CrewListPage = (props: PageTagProps) => {
     const [community3, setCommunity3] = useState([] as any);
     const [communityBR, setCommunityBR] = useState([] as any);
 
-
     useEffect(() => {
         const fetchData = async () => {
             const crewList = await getUserList();
@@ -55,11 +52,6 @@ const CrewListPage = (props: PageTagProps) => {
             setCommunity2(community2);
             setCommunity3(community3);
             setCommunityBR(communityBR);
-
-            console.log(community1.length);
-            console.log(community2.length);
-            console.log(community3.length);
-            console.log(communityBR.length);
 
         }
         fetchData();
@@ -88,11 +80,11 @@ const CrewListPage = (props: PageTagProps) => {
             <Header title={props.title} />
             <main>
                 <SelCommunity>
-                    <Button color={activeButton === -1 ? "secondary" : "outline-secondary"} block onClick={e => selectCommunityList(-1, crewList)}> 전체({crewList.length}) </Button>&nbsp;
-                    <Button color={activeButton === 0 ? "secondary" : "outline-secondary"} block onClick={e => selectCommunityList(0, crewList)}> 1청년부({community1.length}) </Button>&nbsp;
-                    <Button color={activeButton === 1 ? "secondary" : "outline-secondary"} block onClick={e => selectCommunityList(1, crewList)}> 2청년부({community2.length}) </Button>&nbsp;
-                    <Button color={activeButton === 2 ? "secondary" : "outline-secondary"} block onClick={e => selectCommunityList(2, crewList)}> 3청년부({community3.length}) </Button>&nbsp;
-                    <Button color={activeButton === 3 ? "secondary" : "outline-secondary"} block onClick={e => selectCommunityList(3, crewList)}> 신혼BR({communityBR.length}) </Button>
+                    <Button color={activeButton === -1 ? "secondary" : "outline-secondary"} block onClick={e => selectCommunityList(-1, crewList)}> 전체<br />({crewList.length}) </Button>&nbsp;
+                    <Button color={activeButton === 0 ? "secondary" : "outline-secondary"} block onClick={e => selectCommunityList(0, crewList)}> 1청년부<br />({community1.length}) </Button>&nbsp;
+                    <Button color={activeButton === 1 ? "secondary" : "outline-secondary"} block onClick={e => selectCommunityList(1, crewList)}> 2청년부<br />({community2.length}) </Button>&nbsp;
+                    <Button color={activeButton === 2 ? "secondary" : "outline-secondary"} block onClick={e => selectCommunityList(2, crewList)}> 3청년부<br />({community3.length}) </Button>&nbsp;
+                    <Button color={activeButton === 3 ? "secondary" : "outline-secondary"} block onClick={e => selectCommunityList(3, crewList)}> 신혼BR<br />({communityBR.length}) </Button>
                 </SelCommunity>
                 {selectCrewList.map((item: any, index: number) => {
                     return (
